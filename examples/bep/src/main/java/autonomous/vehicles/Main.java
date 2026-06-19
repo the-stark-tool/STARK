6fb888223bee2e1932cd46cfd63d7e309fcf7dc5
stark-tool <150928711+stark-tool@users.noreply.github.com>
2026-06-19 09:26:31 +0200

    Created bep module

---
+++ examples/bep/src/main/java/autonomous/vehicles/Main.java
@@ -0,0 +1,62 @@
+package autonomous.vehicles;
+
+import java.io.IOException;
+
+public class Main {
+
+    /*
+    // Ersoy
+    public static void main(String[] args) throws IOException {
+        try {
+            new name_of_your_main_here();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Arda
+    public static void main(String[] args) throws IOException {
+        try {
+            new name_of_your_main_here();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Joao
+    public static void main(String[] args) throws IOException {
+        try {
+            new name_of_your_main_here();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Daryana
+    public static void main(String[] args) throws IOException {
+        try {
+            new name_of_your_main_here();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Mateusz
+    public static void main(String[] args) throws IOException {
+        try {
+            new name_of_your_main_here();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Michal
+    public static void main(String[] args) throws IOException {
+        try {
+            new name_of_your_main_here();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+    */
+}
