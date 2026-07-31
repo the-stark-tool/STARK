d38d8cbd20f65873c7e15edbb963ed66bb1fad55
jojomaverik <arda.bizeldodo@gmail.com>
2026-07-31 20:09:59 +0300

    Update main.java

--- examples/bep/src/main/java/autonomous/vehicles/Main.java
+++ examples/bep/src/main/java/autonomous/vehicles/Main.java
@@ -14,10 +14,14 @@ public class Main {
         }
     }
 
-    // Arda
+    // Uncomment the case study you would like to run
+    // Case 1 (Primary Case): front gap unsafe, ego slows down first INIT_DISTANCE_BETWEEN = {130, 60}
+    // Case 2: immediate merge INIT_DISTANCE_BETWEEN = {110, 80}
+    // Case 3: borderline unsafe gap INIT_DISTANCE_BETWEEN = {90, 65}
+    // ONE LANE MERGE - ARDA BIZEL
     public static void main(String[] args) throws IOException {
         try {
-            new name_of_your_main_here();
+            new OneLaneMerge();
         } catch (RuntimeException e) {
             e.printStackTrace();
         }
